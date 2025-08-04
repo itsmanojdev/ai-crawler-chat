06ba86744de94007242b5f177d9b43d2fe4d5d55
itsmanojdev <manojscmr333@gmail.com>
2025-08-04 17:12:57 +0530

    AI Logic

--- src/app/crawl/route.js
+++
@@ -1,9 +0,0 @@
-import { crawl } from "../../lib/crawler";
-export async function GET() {
-    try {
-        await crawl();
-        return Response.json({ message: 'Websites Crawled Successfully' });
-    } catch (error) {
-        return Response.json({ error }, { status: 500 });
-    }
-}
--- src/lib/ollama.js
+++ src/lib/ollama.js
@@ -1,4 +1,6 @@
+import embeddingModel from "../models/embeddingModel.js";
 import ollama from 'ollama'
+import mongoose from "mongoose";
 
 const getEmbedding = async (chunk) => {
     try {
@@ -12,6 +14,46 @@ const getEmbedding = async (chunk) => {
     }
 }
 
+const AIChat = async (websiteID, userQuery) => {
+    try {
+        const embedding = await getEmbedding(userQuery)
+
+        let releventChunks = await embeddingModel.aggregate([
+            {
+                $vectorSearch: {
+                    index: 'vector_index', // Replace with your index name
+                    path: 'embedding',
+                    queryVector: embedding,
+                    numCandidates: 100, // Number of candidates to consider
+                    limit: 3, // Number of results to return
+                    filter: {
+                        domain: new mongoose.Types.ObjectId(websiteID)
+                    }
+                }
+            }
+        ]);
+        releventChunks = releventChunks.map(record => record.chunk).join("\n");
+
+
+        let prompt = `Answer the Question only using the content provide.
+            Question: ${userQuery}
+            content: ${releventChunks}
+            `
+        console.log('AI');
+
+        const response = await ollama.chat({
+            model: 'llama3.2',
+            messages: [{ role: 'user', content: prompt }],
+        })
+        console.log(response.message.content);
+
+        return response.message.content
+    } catch (error) {
+        console.log("Error while getting AI response: ", error);
+    }
+}
+
 export {
-    getEmbedding
+    getEmbedding,
+    AIChat
 }
